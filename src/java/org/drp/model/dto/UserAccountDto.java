55e1141b5513f97d01e9dd020b1357d3a80afd7c
Kelechi <anderson.kelechi@yahoo.com>
2018-11-10 21:19:53 -0500

    Data transfer object added for user

---
+++ src/java/org/drp/model/dto/UserAccountDto.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.drp.model.dto;
+
+/**
+ *
+ * @author Kelechi
+ */
+public class UserAccountDto {
+    private String firstname;
+    private String surname;
+    private String email;
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * @param surname the surname to set
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
